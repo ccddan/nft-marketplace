7b2e02b6976360e73724267ad1f4958e099419c6
ccddan <ccddan>
2022-04-03 00:15:03 -0600

    sc:feat: solc configuration

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -26,7 +26,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const ACCOUNTS = process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
