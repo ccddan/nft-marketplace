c1ec41df2877a7667ba72d7633d7ed1ced840f57
ccddan <ccdd.dev@gmail.com>
2022-04-17 15:44:05 -0500

    wc:style: fix hamburguer button focus border color

--- web-client/components/Navbar.tsx
+++ web-client/components/Navbar.tsx
@@ -104,7 +104,7 @@ export const Navbar = () => {
                         <p>NFT Marketplace</p>
                       </div>
                       <div className="-mr-2 flex items-right md:hidden">
-                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-500">
                           <span className="sr-only">Open menu</span>
                           <MenuIcon className="h-6 w-6" aria-hidden="true" />
                         </Popover.Button>
